3343648c69832f425c3ad2c3633d277e07cf42fe
Eduardo <eparra@digitalhouse.com>
2019-11-29 19:40:37 -0300

    Se guarda en la base de datos antes de devolver a la view

--- app/src/main/java/com/dh/repasoviewpagerfactorylima/controlador/ProductoController.java
+++ app/src/main/java/com/dh/repasoviewpagerfactorylima/controlador/ProductoController.java
@@ -6,6 +6,7 @@ import com.dh.repasoviewpagerfactorylima.config.AppDatabase;
 import com.dh.repasoviewpagerfactorylima.dao.ProductoDao;
 import com.dh.repasoviewpagerfactorylima.dao.RoomProductoDao;
 import com.dh.repasoviewpagerfactorylima.modelo.ContenedorProducto;
+import com.dh.repasoviewpagerfactorylima.modelo.Producto;
 import com.dh.repasoviewpagerfactorylima.util.ResultListener;
 
 public class ProductoController {
@@ -22,6 +23,9 @@ public class ProductoController {
         productoDao.obtenerResultadoDao(new ResultListener<ContenedorProducto>() {
             @Override
             public void finish(ContenedorProducto results) {
+                //antes de devolver los resultados a la view, los guardo en la base
+                roomProductoDao.delete();
+                roomProductoDao.insert(results.getResults());
                 escuchadorDeLaVista.finish(results);
             }
         });
