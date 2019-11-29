7f59f2f1a9a1a7918e13b7ccbd21071a6704175c
Eduardo <eparra@digitalhouse.com>
2019-11-29 18:59:32 -0300

    SE AGREGA DAO  PARA Producto Entity

---
+++ app/src/main/java/com/dh/repasoviewpagerfactorylima/dao/RoomProductoDao.java
@@ -0,0 +1,22 @@
+package com.dh.repasoviewpagerfactorylima.dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.dh.repasoviewpagerfactorylima.modelo.Producto;
+
+import java.util.List;
+
+@Dao
+public interface RoomProductoDao {
+
+    @Query("SELECT * FROM Producto")
+    List<Producto> getAll();
+
+    @Insert
+    void insert(Producto... producto);
+
+    @Query("DELETE FROM PRODUCTO")
+    void delete();
+}
